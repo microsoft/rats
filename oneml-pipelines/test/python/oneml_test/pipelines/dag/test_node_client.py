842db54d1dbcd54eee0f250dd873788a5f8a2cd3
Lorenzo Pisani <lopisani@microsoft.com>
2022-08-23 07:31:55 -0700

    migrate existing tests to get 70+% coverage back

--- oneml-pipelines/test/python/oneml_test/pipelines/dag/test_node_client.py
+++ oneml-pipelines/test/python/oneml_test/pipelines/dag/test_node_client.py
@@ -1,12 +1,68 @@
+import pytest
+
 from oneml.pipelines.dag import (
     DuplicatePipelineNodeError,
     ILocatePipelineNodes,
     IManagePipelineNodes,
     IRegisterPipelineNodes,
     NodeNotFoundError,
+    PipelineNode,
     PipelineNodeClient,
 )
 
 
-def test_imports() -> None:
-    assert 1 == 1
+class TestPipelineNodeClient:
+
+    _client: PipelineNodeClient
+
+    def setup(self) -> None:
+        self._client = PipelineNodeClient()
+
+    def test_basics(self) -> None:
+        node1 = PipelineNode("fake-1")
+        node2 = PipelineNode("fake-2")
+        node3 = PipelineNode("fake-3")
+        node4 = PipelineNode("fake-4")
+
+        self._client.register_node(node1)
+        self._client.register_node(node2)
+        self._client.register_node(node3)
+        self._client.register_node(node4)
+
+        assert self._client.get_node_by_key("fake-1") == node1
+        assert self._client.get_node_by_key("fake-2") == node2
+
+    def test_get_nodes(self) -> None:
+        node1 = PipelineNode("fake-1")
+        node2 = PipelineNode("fake-2")
+        node3 = PipelineNode("fake-3")
+        node4 = PipelineNode("fake-4")
+
+        self._client.register_node(node1)
+        self._client.register_node(node2)
+        self._client.register_node(node3)
+        self._client.register_node(node4)
+
+        assert self._client.get_nodes() == tuple([
+            node1,
+            node2,
+            node3,
+            node4,
+        ])
+
+    def test_validation(self) -> None:
+        node1 = PipelineNode("fake-1")
+        node2 = PipelineNode("fake-2")
+        node3 = PipelineNode("fake-3")
+        node4 = PipelineNode("fake-4")
+
+        self._client.register_node(node1)
+        self._client.register_node(node2)
+        self._client.register_node(node3)
+        self._client.register_node(node4)
+
+        with pytest.raises(RuntimeError):
+            self._client.register_node(node1)
+
+        with pytest.raises(RuntimeError):
+            self._client.get_node_by_key("fake-100")
--- oneml-pipelines/test/python/oneml_test/pipelines/session/test_session_state.py
+++ oneml-pipelines/test/python/oneml_test/pipelines/session/test_session_state.py
@@ -7,5 +7,14 @@ from oneml.pipelines.session import (
 )
 
 
-def test_imports() -> None:
-    assert 1 == 1
+class TestPipelineSessionStateClient:
+
+    _client: PipelineSessionStateClient
+
+    def setup(self) -> None:
+        self._client = PipelineSessionStateClient()
+
+    def test_basics(self) -> None:
+        assert self._client.get_state() == PipelineSessionState.PENDING
+        self._client.set_state(PipelineSessionState.RUNNING)
+        assert self._client.get_state() == PipelineSessionState.RUNNING
