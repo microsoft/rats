38bbad3c2418f976ce2ca543bc908bd93aafef2d
Lorenzo Pisani <lopisani@microsoft.com>
2022-08-23 07:23:12 -0700

    export everything from session pipeline package

--- oneml-pipelines/src/python/oneml/pipelines/session/__init__.py
+++ oneml-pipelines/src/python/oneml/pipelines/session/__init__.py
@@ -1,28 +1,112 @@
-from ._executable import CallableExecutable, IExecutable
-from ._node_state import ILocatePipelineNodeState, PipelineNodeState, PipelineNodeStateClient
+from ._executable import (
+    IExecutable,
+    NoOpExecutable,
+    ICallableExecutableProvider,
+    ICallableExecutable,
+    DeferredExecutable,
+    CallableExecutable,
+    ContextType,
+    IContextualCallableExecutable,
+    ContextualCallableExecutable,
+)
+from ._node_execution import (
+    ILocatePipelineNodeExecutables,
+    IRegisterPipelineNodeExecutables,
+    IManagePipelineNodeExecutables,
+    IExecutePipelineNodes,
+    PipelineNodeExecutablesClient,
+    NodeExecutableNotFoundError,
+)
+from ._node_state import (
+    PipelineNodeState,
+    ILocatePipelineNodeState,
+    ISetPipelineNodeState,
+    IManagePipelineNodeState,
+    PipelineNodeStateClient,
+)
+from ._session import (
+    IRunnablePipelineSession,
+    IStoppablePipelineSession,
+    IPipelineSession,
+    PipelineSession,
+)
 from ._session_client import PipelineSessionClient
 from ._session_client_factory import PipelineSessionClientFactory
 from ._session_data_client import (
+    DataType,
     PipelineDataNode,
+    IManagePipelineData,
+    PipelineDataClient,
+    ReadProxyPipelineDataClient,
     PipelineNodeDataClient,
     PipelineNodeDataClientFactory,
 )
+from ._session_frame import (
+    IPipelineSessionFrame,
+    BasicPipelineSessionFrameCommands,
+    PipelineSessionFrame,
+)
 from ._session_plugin_client import PipelineSessionPluginClient
-from ._session_plugins import IPipelineSessionPlugin, IRegisterPipelineSessionPlugins
+from ._session_plugins import (
+    IPipelineSessionPlugin,
+    IRegisterPipelineSessionPlugins,
+    IActivatePipelineSessionPlugins,
+    IManagePipelineSessionPlugins,
+)
+from ._session_state import (
+    PipelineSessionState,
+    ILocatePipelineSessionState,
+    ISetPipelineSessionState,
+    IManagePipelineSessionState,
+    PipelineSessionStateClient,
+)
 
 __all__ = [
     "IExecutable",
-    "PipelineNodeStateClient",
-    "ILocatePipelineNodeState",
+    "NoOpExecutable",
+    "ICallableExecutableProvider",
+    "ICallableExecutable",
+    "DeferredExecutable",
+    "CallableExecutable",
+    "ContextType",
+    "IContextualCallableExecutable",
+    "ContextualCallableExecutable",
+    "ILocatePipelineNodeExecutables",
+    "IRegisterPipelineNodeExecutables",
+    "IManagePipelineNodeExecutables",
+    "IExecutePipelineNodes",
+    "PipelineNodeExecutablesClient",
+    "NodeExecutableNotFoundError",
     "PipelineNodeState",
-    "PipelineNodeDataClient",
+    "ILocatePipelineNodeState",
+    "ISetPipelineNodeState",
+    "IManagePipelineNodeState",
+    "PipelineNodeStateClient",
+    "IRunnablePipelineSession",
+    "IStoppablePipelineSession",
+    "IPipelineSession",
+    "PipelineSession",
+    "DataType",
     "PipelineDataNode",
+    "IManagePipelineData",
+    "PipelineDataClient",
+    "ReadProxyPipelineDataClient",
+    "PipelineNodeDataClient",
     "PipelineNodeDataClientFactory",
-    "PipelineSessionClient",
+    "IPipelineSessionFrame",
+    "BasicPipelineSessionFrameCommands",
+    "PipelineSessionFrame",
+    "IPipelineSessionPlugin",
+    "IRegisterPipelineSessionPlugins",
+    "IActivatePipelineSessionPlugins",
+    "IManagePipelineSessionPlugins",
+    "PipelineSessionState",
+    "ILocatePipelineSessionState",
+    "ISetPipelineSessionState",
+    "IManagePipelineSessionState",
+    "PipelineSessionStateClient",
     "PipelineSessionClientFactory",
     "PipelineSessionPluginClient",
     "CallableExecutable",
     "PipelineSessionClient",
-    "IPipelineSessionPlugin",
-    "IRegisterPipelineSessionPlugins",
 ]
