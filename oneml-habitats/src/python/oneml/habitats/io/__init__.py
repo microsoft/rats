9c9f43f3dceaf246f07be1f927c97fc3f8606dab
Elon Portugaly <sc-qp863@microsoft.com>
2024-02-01 21:50:06 +0000

    remote one-habitats

--- oneml-habitats/src/python/oneml/habitats/io/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._app_plugin import OnemlHabitatsIoPlugin, OnemlHabitatsIoServices
-
-__all__ = [
-    "OnemlHabitatsIoServices",
-    "OnemlHabitatsIoPlugin",
-]
--- oneml-habitats/src/python/oneml/habitats/pipeline_operations/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._app_plugin import (
-    OnemlHabitatsPipelineOperationsPlugin,
-    OnemlHabitatsPipelineOperationsServices,
-)
-
-__all__ = ["OnemlHabitatsPipelineOperationsServices", "OnemlHabitatsPipelineOperationsPlugin"]
