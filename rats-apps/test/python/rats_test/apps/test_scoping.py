cdaa35420b48ce2e38be654797d2d9206c1be60b
Lorenzo Pisani <lopisani@microsoft.com>
2024-04-09 14:46:02 -0700

    just had a bad local env, we can enable these again

--- rats-apps/test/python/rats_test/apps/test_scoping.py
+++ rats-apps/test/python/rats_test/apps/test_scoping.py
@@ -1,40 +1,40 @@
-# from typing import Any
-#
-# from rats import apps
-# from rats.apps import ServiceId
-#
-#
-# @apps.autoscope
-# class ExampleServices:
-#     FOO = apps.ServiceId[Any]("foo")
-#     CONFIG = apps.ConfigId[Any]("config")
-#     OTHER = 52
-#
-#     @staticmethod
-#     def bar(name: str) -> ServiceId[Any]:
-#         return ServiceId[Any](name)
-#
-#     @staticmethod
-#     def custom(name: str) -> str:
-#         return f"prefix[{name}]"
-#
-#
-# class TestScoping:
-#
-#     def test_properties_are_scoped(self) -> None:
-#         expected = f"{__name__}:ExampleServices[foo]"
-#         assert ExampleServices.FOO.name == expected
-#
-#     def test_configs_are_scoped(self) -> None:
-#         expected = f"{__name__}:ExampleServices[config]"
-#         assert ExampleServices.CONFIG.name == expected
-#
-#     def test_functions_are_scoped(self) -> None:
-#         expected = f"{__name__}:ExampleServices[bar]"
-#         assert ExampleServices.bar("bar").name == expected
-#
-#     def test_non_scoped_properties(self) -> None:
-#         assert ExampleServices.OTHER == 52
-#
-#     def test_non_scoped_functions(self) -> None:
-#         assert ExampleServices.custom("foo") == "prefix[foo]"
+from typing import Any
+
+from rats import apps
+from rats.apps import ServiceId
+
+
+@apps.autoscope
+class ExampleServices:
+    FOO = apps.ServiceId[Any]("foo")
+    CONFIG = apps.ConfigId[Any]("config")
+    OTHER = 52
+
+    @staticmethod
+    def bar(name: str) -> ServiceId[Any]:
+        return ServiceId[Any](name)
+
+    @staticmethod
+    def custom(name: str) -> str:
+        return f"prefix[{name}]"
+
+
+class TestScoping:
+
+    def test_properties_are_scoped(self) -> None:
+        expected = f"{__name__}:ExampleServices[foo]"
+        assert ExampleServices.FOO.name == expected
+
+    def test_configs_are_scoped(self) -> None:
+        expected = f"{__name__}:ExampleServices[config]"
+        assert ExampleServices.CONFIG.name == expected
+
+    def test_functions_are_scoped(self) -> None:
+        expected = f"{__name__}:ExampleServices[bar]"
+        assert ExampleServices.bar("bar").name == expected
+
+    def test_non_scoped_properties(self) -> None:
+        assert ExampleServices.OTHER == 52
+
+    def test_non_scoped_functions(self) -> None:
+        assert ExampleServices.custom("foo") == "prefix[foo]"
