d2db17baa6b49899d69bd018dabf589953cd0a5f
Lorenzo Pisani <lopisani@microsoft.com>
2024-04-09 23:30:32 -0700

    small typing fix

--- rats-apps/src/python/rats/apps/_ids.py
+++ rats-apps/src/python/rats/apps/_ids.py
@@ -1,9 +1,10 @@
+import typing
 from typing import Generic, TypeVar
 
 from typing_extensions import NamedTuple
 
 T_ServiceType = TypeVar("T_ServiceType")
-T_ConfigType = TypeVar("T_ConfigType", bound=NamedTuple)
+T_ConfigType = TypeVar("T_ConfigType", bound=typing.NamedTuple)
 Tco_ServiceType = TypeVar("Tco_ServiceType", covariant=True)
 Tco_ConfigType = TypeVar("Tco_ConfigType", bound=NamedTuple, covariant=True)
 
