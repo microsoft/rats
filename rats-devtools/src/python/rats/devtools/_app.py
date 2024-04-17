ecc8bc27c147592240f8ba4024d47ea568a01629
Lorenzo Pisani <lopisani@microsoft.com>
2024-04-16 18:40:55 -0700

    start migration of devtools to apps

--- rats-devtools/src/python/rats/devtools/_app.py
+++ rats-devtools/src/python/rats/devtools/_app.py
@@ -1,33 +1,26 @@
 from typing import Any
 
-from rats.services import (
-    IExecutable,
-    ServiceContainer,
-    ServiceId,
-    scoped_service_ids,
-    service_group,
-    service_provider,
-)
+from rats import apps
 
 from ._click import ClickCommandRegistry
 from ._commands import RatsDevtoolsCli, RatsDevtoolsCommands
 from ._container import DecoratedServiceProvider
 
 
-@scoped_service_ids
+@apps.autoscope
 class RatsDevtoolsAppServices:
-    CLI = ServiceId[IExecutable]("cli")
+    CLI = apps.ServiceId[apps.Executable]("cli")
 
 
 class RatsDevtoolsAppServiceGroups:
-    COMMANDS = ServiceId[ClickCommandRegistry]("commands")
+    COMMANDS = apps.ServiceId[ClickCommandRegistry]("commands")
 
 
 class RatsDevtoolsAppContainer(DecoratedServiceProvider):
     def get_service_ids(self) -> Any:
         raise RuntimeError("deprecated method added for backwards compatibility")
 
-    @service_provider(RatsDevtoolsAppServices.CLI)
+    @apps.service(RatsDevtoolsAppServices.CLI)
     def cli(self) -> RatsDevtoolsCli:
         return RatsDevtoolsCli(
             self.get_service_group_provider(
@@ -35,11 +28,11 @@ class RatsDevtoolsAppContainer(DecoratedServiceProvider):
             )
         )
 
-    @service_group(RatsDevtoolsAppServiceGroups.COMMANDS)
+    @apps.group(RatsDevtoolsAppServiceGroups.COMMANDS)
     def commands(self) -> RatsDevtoolsCommands:
         return RatsDevtoolsCommands()
 
 
 def run() -> None:
-    container = ServiceContainer(RatsDevtoolsAppContainer())
+    container = RatsDevtoolsAppContainer()
     container.get_service(RatsDevtoolsAppServices.CLI).execute()
