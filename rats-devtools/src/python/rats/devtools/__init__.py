9b1aa4e137a1f48df53517a865f482286f11378f
ms-lolo <lopisani@microsoft.com>
2024-04-18 18:33:34 -0700

    feature: devtools app (#116)
    this is the first attempt at using devtools to manage the file watcher
    logic and run formatting tools automatically. this allows us to control
    the list of watchers using the container groups functionality and allows
    us to expose custom messaging to people working in pycharm. i'm fairly
    confident this is possible in vscode but want to experiment here before
    trying to adopt any patterns.

---
+++ rats-devtools/src/python/rats/devtools/__init__.py
@@ -0,0 +1,9 @@
+from ._app import RatsDevtoolsGroups
+from ._click import ClickCommandGroup, ClickCommandRegistry, command
+
+__all__ = [
+    "ClickCommandGroup",
+    "ClickCommandRegistry",
+    "command",
+    "RatsDevtoolsGroups",
+]
---
+++ rats-devtools/src/python/rats/pycharm/_commands.py
@@ -0,0 +1,40 @@
+import subprocess
+import sys
+from pathlib import Path
+
+import click
+
+from rats import devtools
+
+
+class RatsPycharmCommands(devtools.ClickCommandRegistry):
+    @devtools.command
+    @click.argument("filename", type=click.Path(exists=True, file_okay=True, dir_okay=False))
+    def pycharm_actions_on_save(self, filename: str) -> None:
+        """Build the python wheel for one of the components in this project."""
+        component_path = Path(filename.split("/")[0])
+        relative_file = "/".join(filename.split("/")[1:])
+        pyproject_path = Path(f"{component_path}/pyproject.toml")
+
+        if not pyproject_path.is_file():
+            raise ValueError("does not seem to be running within repo root")
+
+        poetry_commands = [
+            ["ruff", "check", "--fix", "--unsafe-fixes", str(relative_file)],
+            ["ruff", "format", str(relative_file)],
+        ]
+
+        try:
+            for cmd in poetry_commands:
+                subprocess.run(
+                    ["poetry", "run", *cmd],
+                    check=True,
+                    cwd=component_path,
+                )
+        except subprocess.CalledProcessError as e:
+            sys.exit(e.returncode)
+
+    @devtools.command
+    def ping(self) -> None:
+        """No-op used for testing."""
+        print("pong")
