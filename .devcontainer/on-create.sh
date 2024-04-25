56d9e555445791c4e2f1d8c0a9ff13c34ab978a5
jzazo <javierzazo@microsoft.com>
2024-04-25 15:04:51 +0000

    🧑‍💻 devxp(devcontainer): added devcontainer config to repo

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# this runs ONCE at devcontainer creation time, and it runs during pre-build phase which
+# means it does not have access to user context
+# ===========================
+
+# finish config of direnv settings (would be nice to improve the devcontainer "feature")
+echo "CONFIGURING DIRENV..."
+echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+eval "$(direnv hook bash)"
+mkdir -p ~/.config/direnv/
+cp /workspaces/rats/.devcontainer/direnvrc ~/.config/direnv/direnvrc
+
+echo "INSTALLING COMMITIZEN"
+pipx install commitizen
+pipx inject commitizen cz-conventional-gitmoji
+
+direnv allow .
+poetry config virtualenvs.in-project true
+
+directories=("rats-apps" "rats-devtools" "rats-pipelines" "rats-processors")
+# Loop through each directory
+for dir in "${directories[@]}"; do
+    echo "POETRY INSTALL in $dir..."
+    cd "$dir" || exit 1
+    direnv allow .
+    poetry install
+    cd - || exit 1
+done
+
+echo "CONFIGURING PRE-COMMIT"
+pre-commit install --install-hooks
