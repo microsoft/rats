1e5a20425cc076799405de5bc6d94227f2938c05
Lorenzo Pisani <lopisani@microsoft.com>
2024-05-03 23:57:48 -0700

    remove duplicated code

--- rats-devtools/src/python/rats/pycharm/_plugin.py
+++ rats-devtools/src/python/rats/pycharm/_plugin.py
@@ -1,30 +1,12 @@
-from dataclasses import dataclass
-from typing import Annotated
-
 import click
 
 # pyright seems to struggle with this namespace package
 # https://github.com/microsoft/pyright/issues/2882
 from rats import apps as apps
 from rats import cli as cli
-from rats import command_tree as command_tree
 from rats import devtools as devtools
 
 from ._commands import PycharmCommands
-
-
-@dataclass(frozen=True)
-class AutoformatterArgs:
-    """Arguments for the autoformatter command."""
-
-    filename: Annotated[
-        str,
-        command_tree.ClickConversion(
-            argument=True,
-            explicit_click_type=click.Path(exists=True, file_okay=True, dir_okay=False),
-        ),
-    ]
-    """The filename to format."""
 
 
 @apps.autoscope
