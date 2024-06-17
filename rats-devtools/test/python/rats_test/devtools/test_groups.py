be4f3638ef28ec0bb223ada26444f41be46b1bea
Lorenzo Pisani <lopisani@microsoft.com>
2024-06-16 23:01:24 -0700

    remove deprecated tests

--- rats-devtools/test/python/rats_test/devtools/test_groups.py
+++ rats-devtools/test/python/rats_test/devtools/test_groups.py
@@ -1,35 +1,8 @@
-from unittest import mock
-from unittest.mock import MagicMock, create_autospec
+from unittest.mock import create_autospec
 
 import click
 
-from rats.apps import PluginRunner
-from rats.cli import AttachClickCommands, ClickExecutable
-
-
-@mock.patch("click.Group")
-def test_command_group_execute_registers_commands(m_click_group: MagicMock):
-    registered_plugins = []
-
-    class MockPlugin:
-        def on_group_open(self, group: click.Group) -> None:
-            registered_plugins.append(self)
-
-    plugin1 = MockPlugin()
-    plugin2 = MockPlugin()
-
-    m_group_instance = m_click_group.return_value
-
-    command_group = ClickExecutable(
-        command=lambda: m_group_instance,
-        plugins=PluginRunner(iter([plugin1, plugin2])),
-    )
-    command_group.execute()
-
-    assert len(registered_plugins) == 2
-    assert plugin1 in registered_plugins
-    assert plugin2 in registered_plugins
-    m_group_instance.assert_called_once()
+from rats.cli import AttachClickCommands
 
 
 def test_group_commands_on_group_registration():
