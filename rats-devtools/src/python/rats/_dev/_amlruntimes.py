9d519277d6992a802125c549e45c325d6adeb06e
Lorenzo Pisani <lopisani@microsoft.com>
2024-08-09 21:32:43 -0700

    move a few more configs around and get aml+k8s submitting again

--- rats-devtools/src/python/rats/_dev/_amlruntimes.py
+++ rats-devtools/src/python/rats/_dev/_amlruntimes.py
@@ -1,4 +1,6 @@
-from rats import apps, amlruntime
+import os
+
+from rats import amlruntime, apps, projects
 
 
 class AmlRuntimePluginContainer(apps.Container):
@@ -15,7 +17,43 @@ class AmlRuntimePluginContainer(apps.Container):
                 amlruntime.PluginServices.AML_ENVIRONMENT_OPS,
             ),
             job_operations=lambda: self._app.get(amlruntime.PluginServices.AML_JOB_OPS),
-            config=lambda: self._app.get(amlruntime.PluginServices.CONFIGS.component_runtime(
-                "rats-devtools",
-            )),
+            config=lambda: self._app.get(
+                amlruntime.PluginServices.CONFIGS.component_runtime(
+                    "rats-devtools",
+                )
+            ),
+        )
+
+    @apps.service(amlruntime.PluginServices.CONFIGS.component_runtime("rats-devtools"))
+    def _devtools_runtime_config(self) -> amlruntime.RuntimeConfig:
+        return self._component_aml_runtime_config("rats-devtools")
+
+    def _component_aml_runtime_config(self, name: str) -> amlruntime.RuntimeConfig:
+        # think of this as a worker node running our executables
+        reg = os.environ.get("DEVTOOLS_K8S_IMAGE_REGISTRY", "default.local")
+        project_tools = self._app.get(projects.PluginServices.PROJECT_TOOLS)
+        context_hash = project_tools.image_context_hash()
+
+        # for now :(
+        cmds = {
+            "rats-devtools": "cd /opt/rats/rats-devtools && bin/rats-devtools aml-runtime worker-node",
+            "rats-examples-minimal": ".venv/bin/python -m rats.minis",
+            "rats-examples-datasets": ".venv/bin/python -m rats.exampledatasets",
+        }
+
+        # a lot more of this needs to be configurable
+        return amlruntime.RuntimeConfig(
+            command=cmds[name],
+            env_variables=dict(),
+            compute=os.environ.get("DEVTOOLS_AMLRUNTIME_COMPUTE", "default"),
+            workspace=amlruntime.AmlWorkspace(
+                subscription_id=os.environ.get("DEVTOOLS_AMLRUNTIME_SUBSCRIPTION_ID", ""),
+                resource_group_name=os.environ.get("DEVTOOLS_AMLRUNTIME_RESOURCE_GROUP", ""),
+                workspace_name=os.environ.get("DEVTOOLS_AMLRUNTIME_WORKSPACE", "default"),
+            ),
+            environment=amlruntime.AmlEnvironment(
+                name=name,
+                image=f"{reg}/{name}:{context_hash}",
+                version=context_hash,
+            ),
         )
--- rats-devtools/src/python/rats/amlruntime/_local_plugin.py
+++ rats-devtools/src/python/rats/amlruntime/_local_plugin.py
@@ -31,25 +31,9 @@
 #             aml_runtime=self._app.get(PluginServices.AML_RUNTIME),
 #         )
 #
-#     @apps.service(PluginServices.AML_RUNTIME)
-#     def _aml_runtime(self) -> apps.Runtime:
-#         try:
-#             return self._app.get(PluginServices.component_runtime(Path().resolve().name))
-#         except apps.ServiceNotFoundError as e:
-#             if e.service_id == PluginServices.component_runtime(Path().resolve().name):
-#                 # this api is confusing
-#                 return apps.NullRuntime()
-#             raise
-#
-
-#
 #     @apps.service(PluginServices.component_runtime("rats-examples-datasets"))
 #     def _datasets_runtime(self) -> AmlRuntime:
 #         return self._aml_component_runtime("rats-examples-datasets")
-#
-#     @apps.service(PluginServices.CONFIGS.component_runtime("rats-devtools"))
-#     def _devtools_runtime_config(self) -> RuntimeConfig:
-#         return self._component_aml_runtime_config("rats-devtools")
 #
 #     @apps.service(PluginServices.CONFIGS.component_runtime("rats-examples-datasets"))
 #     def _datasets_runtime_config(self) -> RuntimeConfig:
@@ -59,42 +43,3 @@
 #     def _minimal_runtime_config(self) -> RuntimeConfig:
 #         return self._component_aml_runtime_config("examples-minimal")
 #
-#     def _component_aml_runtime_config(self, name: str) -> RuntimeConfig:
-#         # think of this as a worker node running our executables
-#         reg = os.environ.get("DEVTOOLS_K8S_IMAGE_REGISTRY", "default.local")
-#         project_tools = self._app.get(projects.PluginServices.PROJECT_TOOLS)
-#         context_hash = project_tools.image_context_hash()
-#
-#         # for now :(
-#         cmds = {
-#             "rats-devtools": "rats-devtools aml-runtime worker-node",
-#             "rats-examples-minimal": "python -m rats.minis",
-#             "rats-examples-datasets": ".venv/bin/python -m rats.exampledatasets",
-#         }
-#
-#         # a lot more of this needs to be configurable
-#         return RuntimeConfig(
-#             command=cmds[name],
-#             env_variables=dict(),
-#             compute=os.environ.get("DEVTOOLS_AMLRUNTIME_COMPUTE", "default"),
-#             workspace=AmlWorkspace(
-#                 subscription_id=os.environ.get("DEVTOOLS_AMLRUNTIME_SUBSCRIPTION_ID", ""),
-#                 resource_group_name=os.environ.get("DEVTOOLS_AMLRUNTIME_RESOURCE_GROUP", ""),
-#                 workspace_name=os.environ.get("DEVTOOLS_AMLRUNTIME_WORKSPACE", "default"),
-#             ),
-#             environment=AmlEnvironment(
-#                 name=name,
-#                 image=f"{reg}/{name}:{context_hash}",
-#                 version=context_hash,
-#             ),
-#         )
-#
-#     @apps.service(PluginServices.AML_CLIENT)
-#     def _aml_client(self) -> MLClient:
-#         workspace = self._app.get(PluginServices.CONFIGS.AML_RUNTIME).workspace
-#         return MLClient(
-#             credential=cast(TokenCredential, DefaultAzureCredential()),
-#             subscription_id=workspace.subscription_id,
-#             resource_group_name=workspace.resource_group_name,
-#             workspace_name=workspace.workspace_name,
-#         )
