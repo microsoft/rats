687b19f8b6cd02aeb12385450e8bd886a4896d41
Lorenzo Pisani <lopisani@microsoft.com>
2024-08-20 12:15:30 -0700

    expose ci.CiCommandsGroup

--- rats-devtools/src/python/rats/ci/__init__.py
+++ rats-devtools/src/python/rats/ci/__init__.py
@@ -1,8 +1,10 @@
 """Commands for building and validating our components."""
 
+from ._commands import CiCommandGroups
 from ._plugin import PluginContainer, PluginServices
 
 __all__ = [
     "PluginContainer",
     "PluginServices",
+    "CiCommandGroups",
 ]
