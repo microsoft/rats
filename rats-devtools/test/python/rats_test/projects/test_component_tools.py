aafcca3e0549dd865d6589ccdf3af0e0e512394c
Lorenzo Pisani <lopisani@microsoft.com>
2024-12-11 19:17:56 -0800

    more work to decouple from poetry

---
+++ rats-devtools/test/python/rats_test/projects/test_component_tools.py
@@ -0,0 +1,17 @@
+from rats import projects
+
+
+class TestComponentTools:
+
+    def test_basics(self) -> None:
+        tools = projects.ProjectTools(lambda: projects.ProjectConfig(
+            name="example-project",
+            path="test/resources/projects",
+            image_registry="none",
+            image_push_on_build=False,
+        ))
+
+        examples = ["pdm", "poetry", "uv"]
+        for x in examples:
+            component = tools.get_component(f"example-{x}")
+            assert component.component_name() == f"example-{x}"
--- rats-devtools/test/resources/projects/example-uv/hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from example-uv!")
-
-
-if __name__ == "__main__":
-    main()
