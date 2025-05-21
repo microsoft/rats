4a91f00f8ce3d6ef278bf93027fc37bb7b52c463
Lorenzo Pisani <lopisani@microsoft.com>
2025-05-21 09:45:53 -0700

    some final touches on aml docs

--- rats-devtools/src/rats_e2e/aml/__init__.py
+++ rats-devtools/src/rats_e2e/aml/__init__.py
@@ -1,4 +1,13 @@
-"""Examples and end-to-end tests for the `rats.aml` package."""
+"""
+Examples and end-to-end tests for the `rats.aml` package.
+
+The [rats_e2e.aml][] module has a small cli application that runs end-to-end tests that can be used
+as examples for different patterns for submitting jobs to aml. It's not exposed as a component
+script, but the cli can be run in the terminal with `python -m rats_e2e.aml` and our basic aml job
+can be run with `python -m rats_e2e.aml basic-job` or with the `rats-aml` command using
+`rats-aml submit rats_e2e.aml.hello-world`. Read the documentation for the example jobs to dive
+deeper into ways to create and run aml jobs.
+"""
 
 from ._app import Application, main
 
