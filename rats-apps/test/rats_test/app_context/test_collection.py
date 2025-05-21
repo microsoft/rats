3d1cbc9554f89efd96c57738764f744520fabed8
Lorenzo Pisani <lopisani@microsoft.com>
2025-05-21 00:34:04 -0700

    basic tests for rats.app_context

---
+++ rats-apps/test/rats_test/app_context/test_collection.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from rats import app_context, apps
+
+
+@dataclass(frozen=True)
+class ExampleConfig:
+    name: str
+    value: dict[str, str]
+
+
+@apps.autoscope
+class TestServices:
+    CONTEXT_1 = apps.ServiceId[ExampleConfig]("context-1")
+
+
+class TestCollection:
+    _context: ExampleConfig
+    _collection: app_context.Collection
+
+    def setup_method(self) -> None:
+        self._context = ExampleConfig("example1", {"a": "b"})
+        self._collection = app_context.Collection.make(
+            app_context.Context.make(TestServices.CONTEXT_1, self._context),
+        )
+
+    def test_basic_serialization(self) -> None:
+        json_string = app_context.dumps(self._collection)
+        print(json_string)
+        loaded = app_context.loads(json_string)
+        ctx = loaded.decoded_values(ExampleConfig, TestServices.CONTEXT_1)
+        assert ctx[0] == self._context
