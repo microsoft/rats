f15a8f627cf14d970f6a4e9d6025d9335e4f60d3
Lorenzo Pisani <lopisani@microsoft.com>
2025-06-25 10:15:31 -0700

    add a test for single-component repos

--- rats-devtools/test/rats_test/projects/test_project_tools.py
+++ rats-devtools/test/rats_test/projects/test_project_tools.py
@@ -13,3 +13,15 @@ class TestProjectTools:
         )
 
         assert len(tools.discover_components()) == 3
+
+    def test_single_component_projects(self) -> None:
+        tools = projects.ProjectTools(
+            lambda: projects.ProjectConfig(
+                name="example-project",
+                path="test/rats_test_resources/projects/example-2",
+                image_registry="none",
+                image_push_on_build=False,
+            )
+        )
+
+        assert len(tools.discover_components()) == 1
